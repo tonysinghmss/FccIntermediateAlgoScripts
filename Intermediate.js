8e5bf9998eb6425c5adcd7bd7b9b8fc3bc32e183
Manishankar Singh <manishankarsngh007@gmail.com>
2016-12-03 21:33:04 +0530

    Drop it done

--- Intermediate.js
+++ Intermediate.js
@@ -1,4 +1,20 @@
 //==================================================================
+//Drop it
+function dropElements(arr, func) {
+  // Drop them elements.
+  while(arr.length>0){
+    if(func(arr[0])){
+      break;
+    }
+    else{
+       arr.shift();
+    }
+  }
+  return arr;
+}
+
+dropElements([1, 2, 3,4], function(n) {return n >= 3; });
+//==================================================================
 //Finder keepers
 function findElement(arr, func) {
   var num = 0;
